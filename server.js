5577f4a2fade60e6c4811a8af1140ca9459c6951
Victory Njoku <njokuvictory216@gmail.com>
2023-09-04 10:17:33 +0100

    refactoring

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const port = parseInt(process.env.PORT, 10) || 5000;
 
 const app = express();
 
-<<<<<<< HEAD
 app.use(express.json());
 app.use('/', router);
 
@@ -14,6 +13,3 @@ app.listen(port, () => {
 });
 
 export default app;
-=======
-export default server;
->>>>>>> 7ac1a02f6e40b89c1c0e8db5098d731a02862cb5
